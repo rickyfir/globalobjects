a54933aae2a0631290028e286e013fb69ebec3bd
rickyfir <58960435+rickyfir@users.noreply.github.com>
2022-11-28 00:41:04 +0700

    first hello world

---
+++ hello-world/hello-world.js
@@ -0,0 +1,10 @@
+(function () {
+	'use strict';
+	/* Start of your code */
+	function greetMe(JavaScript) {
+		alert(`Hello ${JavaScript}`);
+	}
+
+	greetMe('World');
+	/* End of your code */
+})();
